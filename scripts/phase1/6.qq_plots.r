4fa3112667cac434412db3f52fe5c9a7aaf85739
Joana Viana <j.viana@bham.ac.uk>
2021-09-07 11:39:08 +0100

    linear regression results and qq-plot

---
+++ scripts/phase1/6.qq_plots.r
@@ -0,0 +1,19 @@
+#This script plots the qq-plot for the linear regression results .
+
+input.data.dir <- "~/Documents/test/mitra/data/" #set input data directory
+output.plots <- "~/Documents/test/mitra/plots/" #set output data directory
+
+
+linreg<- read.csv(paste0(input.data.dir, "alien_linear_regression.csv"), row.names = 1)
+
+
+#we -log10 transform the pvalues and theoretical quantiles se we can visualise the most significant pvalues well.
+logp <- -log10(linreg$pvalue)
+logt <- -log10(1:nrow(linreg)/nrow(linreg)) # calculate theoretical quantiles for qq plot
+
+
+#plot qqplot
+pdf(paste0(output.plots, "qq_linear_regression.pdf"))
+	plot(logt, logp, xlab="Theoretical quantiles", ylab="Experimental quantiles", main="QQ-plot linear regresion")
+	abline(a=0, b=1, col='red')
+dev.off()
