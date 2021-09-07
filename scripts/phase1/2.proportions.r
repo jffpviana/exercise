c2bb89752f1cb4a942a80ef8ffc578da8da1f3d7
Joana Viana <j.viana@bham.ac.uk>
2021-09-07 10:00:50 +0100

    fix typo and remove reduntant

--- scripts/phase1/2.proportions.r
+++ scripts/phase1/2.proportions.r
@@ -9,17 +9,17 @@ output.data.dir <- "~/Documents/test/mitra/data/" #set output data directory
 function.source <- "~/Documents/test/mitra/functions/proportionA.r"
 
 
-alian.data <- read.csv(paste0(input.data.dir, "alian_data_formatted.csv"), row.names = 1)
+alien.data <- read.csv(paste0(input.data.dir, "alien_data_formatted.csv"), row.names = 1)
 
-alian.meta <- read.csv(paste0(input.metadata.dir, "alian_metadata.csv"), row.names = 1)
+alien.meta <- read.csv(paste0(input.metadata.dir, "alien_metadata.csv"), row.names = 1)
 
 #load function to calculate the percentage of A for a single positions
 source(function.source)
 
 #apply the new function to all rows of the data
-t(apply(alian.data, 1, prop_mod, samples=alian.meta))-> propA
+t(apply(alien.data, 1, prop_mod, samples=alien.meta))-> propA
 
-#propA is a dataframe with the proportions of modification A in each sample, in each position of the alian genome
+#propA is a dataframe with the proportions of modification A in each sample, in each position of the alien genome
 
 #save the proportions dataframe
-write.csv(propA, file=paste0(output.data.dir, "alian_proportionA.csv"))
+write.csv(propA, file=paste0(output.data.dir, "alien_proportionA.csv"))
--- scripts/phase1/3.coverage.r
+++ scripts/phase1/3.coverage.r
@@ -9,17 +9,17 @@ output.data.dir <- "~/Documents/test/mitra/data/" #set output data directory
 function.source <- "~/Documents/test/mitra/functions/coverage.r"
 
 
-alian.data <- read.csv(paste0(input.data.dir, "alian_data_formatted.csv"), row.names = 1)
+alien.data <- read.csv(paste0(input.data.dir, "alien_data_formatted.csv"), row.names = 1)
 
-alian.meta <- read.csv(paste0(input.metadata.dir, "alian_metadata.csv"), row.names = 1)
+alien.meta <- read.csv(paste0(input.metadata.dir, "alien_metadata.csv"), row.names = 1)
 
 #load function to calculate the site coverage
 source(function.source)
 
 #apply the new function to all rows of the data
-t(apply(alian.data, 1, cov_counts, samples=alian.meta))-> total.cov
+t(apply(alien.data, 1, cov_counts, samples=alien.meta))-> total.cov
 
-#total.cov is a dataframe with the total number of counts in each sample, in each position of the alian genome
+#total.cov is a dataframe with the total number of counts in each sample, in each position of the alien genome
 
 #save the coverage dataframe
-write.csv(total.cov, file=paste0(output.data.dir, "alian_total_coverage.csv"))
+write.csv(total.cov, file=paste0(output.data.dir, "alien_total_coverage.csv"))
