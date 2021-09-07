cc088e7b56167e3c0d4a48c954d7e5ee2b4c8478
Joana Viana <j.viana@bham.ac.uk>
2021-09-07 10:25:17 +0100

    exploratory plots

--- scripts/phase1/4.exploring_data.r
+++ scripts/phase1/4.exploring_data.r
@@ -3,16 +3,18 @@
 input.data.dir <- "~/Documents/test/mitra/data/" #set input data directory
 input.metadata.dir <- "~/Documents/test/mitra/metadata/" #set input metadata directory
 output.data.dir <- "~/Documents/test/mitra/data/" #set output data directory
-
-
-alian.data <- read.csv(paste0(input.data.dir, "alian_proportionA.csv"), row.names = 1)
-
-alian.meta <- read.csv(paste0(input.metadata.dir, "alian_metadata.csv"), row.names = 1)
-
-
-#plot the histogram and density plot for the data. This will give us an idea of the overall distribution of the data
+output.plots <- "~/Documents/test/mitra/plots/" #set output plots directory
 
 
 
+alien.data <- read.csv(paste0(input.data.dir, "alien_proportionA.csv"), stringsAsFactors = FALSE, row.names = 1)
 
-#plot the histogram and density plot for a few random sites in the alien genome to look at the distribution of those sites
+alien.meta <- read.csv(paste0(input.metadata.dir, "alien_metadata.csv"), row.names = 1)
+
+
+#plot the histogram and density plots for the data. This will give us an idea of the overall distribution of the data
+
+pdf(paste0(output.plots, "modifications_distribution.pdf"))
+  hist(as.matrix(alien.data), main='Histogram', xlab='Modification A proportion') #plot the histogram of the proportion of modificaion A
+  plot(density(as.matrix(alien.data)), col='red', main='Density', xlab='Modification A proportion') #plot the density of the proportion of modificaion A
+dev.off()
--- scripts/phase1/5.linear_regression.r
+++ scripts/phase1/5.linear_regression.r
@@ -3,9 +3,9 @@ input.metadata.dir <- "~/Documents/test/mitra/metadata/" #set input metadata dir
 output.data.dir <- "~/Documents/test/mitra/data/" #set output data directory
 
 
-propA<- read.csv(paste0(input.data.dir, "alian_proportionA.csv"), row.names = 1)
+propA<- read.csv(paste0(input.data.dir, "alien_proportionA.csv"), row.names = 1)
 
-alian.meta <- read.csv(paste0(input.metadata.dir, "alian_metadata.csv"), row.names = 1)
+alien.meta <- read.csv(paste0(input.metadata.dir, "alien_metadata.csv"), row.names = 1)
 
 
 linreg.results <- matrix(NA, nrow=nrow(propA), ncol=4)
@@ -14,7 +14,7 @@ rownames(linreg.results) <- rownames(propA)
 
 
 for(i in 1:nrow(propA)){
-	model<-lm(unlist(propA[i, ]) ~ as.factor(alian.meta$region))
+	model<-lm(unlist(propA[i, ]) ~ as.factor(alien.meta$region))
 
   linreg.results[i, "Estimate"] <- summary(model)$coefficients[2,"Estimate"]
   linreg.results[i, "SE"] <- summary(model)$coefficients[2,"Std. Error"]
